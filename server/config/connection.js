5d1bd4eeb5ff0a723a3070df07ac9eb9a3ef4bec
Ashley Kohl <akohl22@gmail.com>
2021-07-18 22:56:37 -0600

    new heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/lit-stream', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/dry-brushlands', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
