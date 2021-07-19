be6b81e3ff88e0ea363712e64413fe008779e6f3
Ashley Kohl <akohl22@gmail.com>
2021-07-18 19:20:18 -0600

    trying to get savedBook to work

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -69,20 +69,32 @@ const SearchBooks = () => {
       return false;
     }
 
+    // try {
+    //   console.log(bookToSave);
+    //   const response = await saveBook({
+    //     variables: { bookData: { ...bookToSave } },
+    //         });
+
+    //   if (!response.ok) {
+    //     throw new Error('something went wrong!');
+    //   }
+
+    //   // if book successfully saves to user's account, save book id to state
+    //   setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    // } catch (err) {
+    //   console.error(err);
+    // }
     try {
-      console.log(bookToSave);
-      const response = await saveBook(bookToSave, token);
-     
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      // if book successfully saves to user's account, save book id to state
+      const { data } = await saveBook({
+        variables: { bookData: { ...bookToSave } },
+      });
+      console.log('data:', data);
+      console.log(savedBookIds);
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
     }
+    
   };
 
   return (
